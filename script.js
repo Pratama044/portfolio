8b8b25c3d27c843fd3bd0d647854588262d6132e
Kaizen <121414529+Pratama044@users.noreply.github.com>
2023-01-11 17:11:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// function openNav(){
+//     if (document.getElementById('mysidebar').style.width== '200px'){
+//     document.getElementById('mysidebar').style.width= '0';
+//     }else{
+//         document.getElementById('mysidebar').style.width= '200px';
+//     }
+
+
+// }
+
+function openNav(){
+    document.getElementById('mysidebar').style.width= '200px';
+
+}
+function closeNav(){
+    document.getElementById('mysidebar').style.width= '0';
+
+}
